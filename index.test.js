eaaad57c8e8a2de6b9a8c29d21dc5ff85e1d8dbf
Piotr Bryla <piotr.bryla@newday.co.uk>
2024-02-02 14:22:53 +0000

    refactor: use .each syntax

--- index.test.js
+++ index.test.js
@@ -1,7 +1,21 @@
 // hello!
 
+// A .-            N -.
+// B -...          O ---
+// C -.-.          P .--.
+// D -..           Q --.-
+// E .             R .-.
+// F ..-.          S ...
+// G --.           T -
+// H ....          U ..-
+// I ..            V ...-
+// J .---          W .--
+// K -.-           X -..-
+// L .-..          Y -.--
+// M --            Z --..
+
 const morseCodeToLatinLetter = (morseChar) => {
-  switch(morseChar) {
+  switch (morseChar) {
     case ".-":
       return "A";
     case "-...":
@@ -10,8 +24,13 @@ const morseCodeToLatinLetter = (morseChar) => {
 };
 
 describe("Morse code to latin alphabet", () => {
-  it("should convert Morse code char to latin letter for A and B only", () => {
-    expect(morseCodeToLatinLetter(".-")).toBe("A");
-    expect(morseCodeToLatinLetter("-...")).toBe("B");
-  });
+  it.each([
+    { morse: ".-", latin: "A" },
+    { morse: "-...", latin: "B" },
+  ])(
+    "should convert Morse code char $morse to latin letter for $latin",
+    ({ morse, latin }) => {
+      expect(morseCodeToLatinLetter(morse)).toBe(latin);
+    }
+  );
 });
