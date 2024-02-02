8f887b2aea9cce1d471f0b43b27ced892dc14c8c
Piotr Bryla <piotr.bryla@newday.co.uk>
2024-02-02 14:45:42 +0000

    refactor: support full alphabet

--- index.test.js
+++ index.test.js
@@ -15,18 +15,66 @@
 // M --            Z --..
 
 const morseCodeToLatinLetter = (morseChar) => {
-  switch (morseChar) {
-    case ".-":
-      return "A";
-    case "-...":
-      return "B";
-  }
+  const map = {
+    ".-": "A",
+    "-.": "N",
+    "-...": "B",
+    "---": "O",
+    "-.-.": "C",
+    ".--.": "P",
+    "-..": "D",
+    "--.-": "Q",
+    ".": "E",
+    ".-.": "R",
+    "..-.": "F",
+    "...": "S",
+    "--.": "G",
+    "-": "T",
+    "....": "H",
+    "..-": "U",
+    "..": "I",
+    "...-": "V",
+    ".---": "J",
+    ".--": "W",
+    "-.-": "K",
+    "-..-": "X",
+    ".-..": "L",
+    "-.--": "Y",
+    "--": "M",
+    "--..": "Z",
+  };
+
+  return map[morseChar];
 };
 
 describe("Morse code to latin alphabet", () => {
   it.each([
     { morse: ".-", latin: "A" },
+    { morse: "-.", latin: "N" },
     { morse: "-...", latin: "B" },
+    { morse: "---", latin: "O" },
+    { morse: "-.-.", latin: "C" },
+    { morse: ".--.", latin: "P" },
+    { morse: "-..", latin: "D" },
+    { morse: "--.-", latin: "Q" },
+    { morse: ".", latin: "E" },
+    { morse: ".-.", latin: "R" },
+    { morse: "..-.", latin: "F" },
+    { morse: "...", latin: "S" },
+    { morse: "--.", latin: "G" },
+    { morse: "-", latin: "T" },
+    { morse: "....", latin: "H" },
+    { morse: "..-", latin: "U" },
+    { morse: "..", latin: "I" },
+    { morse: "...-", latin: "V" },
+    { morse: ".---", latin: "J" },
+    { morse: ".--", latin: "W" },
+    { morse: "-.-", latin: "K" },
+    { morse: "-..-", latin: "X" },
+    { morse: ".-..", latin: "L" },
+    { morse: "-.--", latin: "Y" },
+    { morse: "--", latin: "M" },
+    { morse: "--..", latin: "Z" },
   ])(
     "should convert Morse code char $morse to latin letter for $latin",
     ({ morse, latin }) => {
